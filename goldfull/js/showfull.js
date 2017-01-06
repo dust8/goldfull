4e656393b8d24f399cab38ce640f0c2bbf3e8c5a
tom <dust8@users.noreply.github.com>
2017-01-06 21:59:01 +0800

    release 1.2

--- goldfull/js/showfull.js
+++ goldfull/js/showfull.js
@@ -1,13 +1,19 @@
-function cleanShowFullBlock() {
+function cleanShowFull() {
     var showFullBlocks = document.getElementsByClassName("show-full-block");
     if (showFullBlocks.length > 0) {
+        // remove .show-full-block div
         var showFullBlock = showFullBlocks[0];
         showFullBlock.parentNode.removeChild(showFullBlock);
+
+        // remove .show-full div
+        var showFulls = document.getElementsByClassName("show-full");
+        var showFull = showFulls[0];
+        showFull.parentNode.removeChild(showFull);
     }
 }
 
 document.onreadystatechange = function() {
     if (document.readyState === "complete") {
-        cleanShowFullBlock();
+        cleanShowFull();
     }
 };
